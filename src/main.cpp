6070cbc67813e937c60709cbcc22b767a56d7024
Juan S. Marquerie <50079201+JsMarq96@users.noreply.github.com>
2023-04-17 16:13:01 +0200

    Added dependencies & cmake

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <webgpu-headers/webgpu.h>
+#include <wgpu.h>
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+#define WGPU_TARGET_MACOS 1
+#define WGPU_TARGET_LINUX_X11 2
+#define WGPU_TARGET_WINDOWS 3
+#define WGPU_TARGET_LINUX_WAYLAND 4
+
+#if WGPU_TARGET == WGPU_TARGET_MACOS
+#include <Foundation/Foundation.h>
+#include <QuartzCore/CAMetalLayer.h>
+#endif
+
+#include <GLFW/glfw3.h>
+#if WGPU_TARGET == WGPU_TARGET_MACOS
+#define GLFW_EXPOSE_NATIVE_COCOA
+#elif WGPU_TARGET == WGPU_TARGET_LINUX_X11
+#define GLFW_EXPOSE_NATIVE_X11
+#elif WGPU_TARGET == WGPU_TARGET_LINUX_WAYLAND
+#define GLFW_EXPOSE_NATIVE_WAYLAND
+#elif WGPU_TARGET == WGPU_TARGET_WINDOWS
+#define GLFW_EXPOSE_NATIVE_WIN32
+#endif
+#include <GLFW/glfw3native.h>
+
+WGPUInstance instance = NULL;
+
+
+int main() {
+    std::cout << "Hello" << std::endl;
+    return 0;
+}
